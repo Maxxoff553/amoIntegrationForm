64a173e1a771376f6652edeb1c8f9e781c043a47
maksimS <olegkovalev553@gmail.com>
2025-12-10 23:09:34 +0300

    поправил метод для получения токена

--- core/amo/AmoCrmClass.php
+++ core/amo/AmoCrmClass.php
@@ -43,41 +43,8 @@ class AmoCrmClass {
             ];
         }
 
-        $curl = curl_init();
-        curl_setopt($curl,CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl,CURLOPT_USERAGENT,'amoCRM-oAuth-client/1.0');
-        curl_setopt($curl,CURLOPT_URL, $link);
-        curl_setopt($curl,CURLOPT_HTTPHEADER,['Content-Type:application/json']);
-        curl_setopt($curl,CURLOPT_HEADER, false);
-        curl_setopt($curl,CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($curl,CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($curl,CURLOPT_SSL_VERIFYPEER, 1);
-        curl_setopt($curl,CURLOPT_SSL_VERIFYHOST, 2);
-        $output = curl_exec($curl);
-        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        curl_close($curl);
+        $response = json_decode($this->curlRequest($link, $data), true);
 
-        $code = (int)$code;
-        $errors = [
-            400 => 'Bad request',
-            401 => 'Unauthorized',
-            403 => 'Forbidden',
-            404 => 'Not found',
-            500 => 'Internal server error',
-            502 => 'Bad gateway',
-            503 => 'Service unavailable',
-        ];
-
-        try {
-            if ($code < 200 || $code > 204) {
-                throw new Exception($errors[$code] ?? 'Undefined error', $code);
-            }
-        } catch(Exception $e) {
-            echo $output;
-            die('Ошибка: ' . $e->getMessage() . PHP_EOL . 'Код ошибки: ' . $e->getCode());
-        }
-
-        $response = json_decode($output, true);
         $this->access_token = $response['access_token'];
 
         $token = [
@@ -90,7 +57,7 @@ class AmoCrmClass {
         file_put_contents(AmoCrmClass::TOKEN_FILE, json_encode($token));
     }
 
-    private function tokenCurlRequest($link, $PostFields = []): string {
+    private function curlRequest($link, $PostFields = []): string {
         $headers = [
             'Authorization: Bearer ' . $this->access_token,
             'Content-Type: application/json'
@@ -138,8 +105,8 @@ class AmoCrmClass {
         $result = '';
         try {
             $url = 'https://' . AmoCrmClass::SUB_DOMAIN . '.amocrm.ru/api/v4/' . $service;
-            $result = json_decode($this->tokenCurlRequest($url, $params), true);
-        } catch (ErrorException $e) {
+            $result = json_decode($this->curlRequest($url, $params), true);
+        } catch (Exception $e) {
             $this->Error($e);
         }
 
